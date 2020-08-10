0050efeeec34536b468549978c36fedc427472a8
Doğukan Batal <dogukanbatal@gmail.com>
2020-08-10 17:24:16 +0300

    Footer fixed

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,11 +1,18 @@
 <template>
-  <div class="container container-wide section-gap">
-    <footer class="footer">
-      <div class="text">Developer by <a href="">Doğukan Batal</a></div>
-      <div class="text text-icon">
-        <a href=""><GithubIcon size="1.2x" class="icon" />Github Repository</a>
-      </div>
-    </footer>
+  <div class="footer-wrapper">
+    <div class="container container-wide section-gap">
+      <footer class="footer">
+        <div class="text">
+          Developed by
+          <a href="http://linkedin.com/in/dogukanbatal">Doğukan Batal</a>
+        </div>
+        <div class="text text-icon">
+          <a href="https://github.com/dogukanbatal/covid19-tracker/"
+            ><GithubIcon size="1.2x" class="icon" />Github Repository</a
+          >
+        </div>
+      </footer>
+    </div>
   </div>
 </template>
 
@@ -31,11 +38,19 @@ export default {
   border-top-left-radius: var(--border-radius);
   border-top-right-radius: var(--border-radius);
   box-shadow: 0 -25px 35px rgba(0, 0, 0, 0.07);
+  &-wrapper {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    margin: auto;
+  }
   @media (--m) {
     flex-direction: column-reverse;
     justify-content: center;
     height: auto;
-    padding: 15px 10px;
+    padding: 10px;
+    border-radius: 0;
     .text-icon {
       margin-bottom: 5px;
     }
@@ -43,6 +58,7 @@ export default {
   .text {
     color: rgb(var(--gray-1));
     font-weight: 500;
+    font-size: 13px;
     a {
       &:hover {
         color: rgb(var(--gray-3));
